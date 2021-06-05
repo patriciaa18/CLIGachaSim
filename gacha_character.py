7c239ef3e22fbab612113c6f184f03d5bd704c28
Patricia <64893306+patriciaa18@users.noreply.github.com>
2021-06-05 21:28:18 +0700

    Update gacha_character.py

--- gacha_character.py
+++ gacha_character.py
@@ -4,7 +4,7 @@ import numpy as np
 class GachaCharacter:
     def __init__(self):
         self.gachaSimulation()
-
+        
     def gachaSimulation(self):
         gacha_item = np.array(['(*5) Eula', '(*5) Jean', '(*5) Diluc','(*5) Qiqi', '(*5) Keqing', '(*5) Mona',
         '(*4) Beidou', '(*4) Xingqiu', '(*4) Xinyan',
@@ -16,33 +16,32 @@ class GachaCharacter:
         '(*3) Cool Steel', '(*3) Ferrous Shadow', '(*3) Slingshot', '(*3) Bloodstained Greatsword', 
         '(*3) Magic Guide', '(*3) Thrilling Tales of Dragon Slayers', '(*3) Dark Iron Sword', '(*3) Fillet Blade'])
         
-        #probability, sum equals one
-        prob = np.array([0.0016666666, 0.0016666666, 0.0016666666, 0.0016666666, 0.0016666666, 0.0016666666,
-        0.01333333333, 0.01333333333, 0.01333333333,
-        0.00133333333, 0.00133333333, 0.00133333333, 0.00133333333, 0.00133333333, 0.00133333333,
-        0.00133333333, 0.00133333333, 0.00133333333, 0.00133333333, 0.00133333333,
-        0.00133333333, 0.00133333333, 0.00133333333, 0.00133333333,
-        0.0583125, 0.0583125, 0.0583125, 0.0583125,
-        0.0583125, 0.0583125, 0.0583125, 0.0583125,
-        0.0583125, 0.0583125, 0.0583125, 0.0583125,
-        0.0583125, 0.0583125, 0.0583125, 0.0583125])
+        prob = np.array([0.001, 0.001, 0.001, 0.001, 0.001, 0.001,
+        0.0103333333333333, 0.0103333333333333, 0.0103333333333333,
+        0.0013333333333333, 0.0013333333333333, 0.0013333333333333, 0.0013333333333333, 0.0013333333333333, 0.0013333333333333,
+        0.0013333333333333, 0.0013333333333333, 0.0013333333333333, 0.0013333333333333, 0.0013333333333333,
+        0.0013333333333333, 0.0013333333333333, 0.0013333333333333, 0.0013333333333333,
+        0.0589375, 0.0589375, 0.0589375, 0.0589375,
+        0.0589375, 0.0589375, 0.0589375, 0.0589375,
+        0.0589375, 0.0589375, 0.0589375, 0.0589375,
+        0.0589375, 0.0589375, 0.0589375, 0.0589375])
+        
+        roll = int(input("How much roll do you want?"))
 
-        #how much player want to pull
-        roll = int(input("How much do roll you want?"))
-
-        #save inital time
         initialTime = time.time()
 
-        #randomize by roll and probability
         gacha = np.random.choice(gacha_item, roll, p=prob)
-        print(gacha)
+
+        gacha_item, counts = np.unique(gacha, return_counts=True)
+        for item, count in zip(gacha_item, counts):
+            print(item, count)
+
         print("-----------------------------------------------------------------")
-        
-        #print time consumed
+
         print("Time taken: ", time.time() - initialTime, " seconds")
+
         self.gachaAgain()
 
-    #gacha again
     def gachaAgain(self):
         print("Roll again?")
         yn = input('yes or no: ')
