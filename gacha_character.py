a63e3cffab217957058c8d5d0ee7ccc69408ed53
patriciaa18 <64893306+patriciaa18@users.noreply.github.com>
2021-06-05 12:12:09 +0700

    Update gacha_character.py

--- gacha_character.py
+++ gacha_character.py
@@ -2,7 +2,6 @@ import time
 import numpy as np
 
 class GachaCharacter:
-    #inisialisasi, memanggil fungsi gachaSimulation
     def __init__(self):
         self.gachaSimulation()
 
@@ -17,7 +16,7 @@ class GachaCharacter:
         '(*3) Cool Steel', '(*3) Ferrous Shadow', '(*3) Slingshot', '(*3) Bloodstained Greatsword', 
         '(*3) Magic Guide', '(*3) Thrilling Tales of Dragon Slayers', '(*3) Dark Iron Sword', '(*3) Fillet Blade'])
         
-        #sesuai urutan, masukkan probabilitas masing-masing elemen dalam desimal, jumlah semua probabilitas harus = 1
+        #probability, sum equals one
         prob = np.array([0.00166666666, 0.00166666666, 0.00166666666, 0.00166666666, 0.00166666666, 0.00166666666,
         0.01666666667, 0.01666666667, 0.01666666667,
         0.00266666666, 0.00266666666, 0.00266666666, 0.00266666666, 0.00266666666, 0.00266666666,
@@ -28,40 +27,22 @@ class GachaCharacter:
         0.05625, 0.05625, 0.05625, 0.05625,
         0.05625, 0.05625, 0.05625, 0.05625])
 
-        #mengambil seberapa banyak roll yang diinginkan pemain
+        #how much player want to pull
         roll = int(input("How much do roll you want?"))
 
-        #mengambil waktu sebelum gacha dilakukan
+        #save inital time
         initialTime = time.time()
 
-        #untuk mengundi (gacha) dari gacha item sebanyak roll dan peluang sesuai prob
+        #randomize by roll and probability
         gacha = np.random.choice(gacha_item, roll, p=prob)
-
-        #perulangan untuk print hasil gacha 
-        # for i, gacha_result in enumerate(gacha):
-        #     self.print_gacha_result(i, gacha_result)
         print(gacha)
         print("-----------------------------------------------------------------")
-        #print waktu sekarang dikurangi waktu sebelumnya untuk mendapatkan lama program dijalankan
+        
+        #print time consumed
         print("Time taken: ", time.time() - initialTime, " seconds")
-        #tanya apakah ingin gacha lagi
         self.gachaAgain()
 
-    #fungsi untuk mencetak hasil gacha dengan warna dan ditambahi angka
-    #warna kuning untuk *5
-    #warna ungu untuk *4
-    #warna biru(cyan) untuk *3
-    # def print_gacha_result(i, gacha_result, self):
-    #     gacha_result = gacha_result
-    #     if '(*5)' in gacha_result:
-    #         return print( str(i+1) + '.', '\033[33m' + gacha_result + '\x1b[0m')
-    #     elif '(*4)' in gacha_result:
-    #         return print(str(i+1) + '.', '\033[35m' + gacha_result + '\x1b[0m')
-    #     else:
-    #         return print(str(i+1) + '.', '\033[36m' + gacha_result + '\x1b[0m')
-
-    # menanyakan apakah ingin gacha kembali
-    # jika jawaban yes maka ulangi gacha jika no permainan berhenti
+    #gacha again
     def gachaAgain(self):
         print("Roll again?")
         yn = input('yes or no: ')
